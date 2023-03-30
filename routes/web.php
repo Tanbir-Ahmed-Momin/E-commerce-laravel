1bd7e54ca9848a594688a2af1be0a22af8b3a379
Tanbir-Ahmed-Momin <tanbir15-12844@diu.edu.bd>
2023-03-31 02:41:59 +0600

    admin panel design added from bootstrap template

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ContractController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\HelloController;
-use App\Http\Controllers\LoginController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,26 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::view('/','welcome');
-Route::get('/', [DashboardController::class,'dashboard']);
-Route::get('/about-company', [AboutController::class,'about'])->name('about');
-Route::get('/contract', [ContractController::class,'contract'])->name('contract');
-Route::get('/login-user', [LoginController::class,'login'])->name('login');
+ Route::get('/',function(){
+    return view('welcome');
+ });
 
-// Route::get('/hello', [HelloController::class,'index']);
-
-//category crud
-Route::controller(CategoryController::class)->name('category.')->prefix('category')->group(function(){
-Route::get('/', 'index')->name('index');
-Route::get('/create', 'create')->name('create');
-Route::post('/create', 'store')->name('store');
-Route::get('/edit', 'edit')->name('edit');
-Route::get('/show','show')->name('show');
-Route::post('/update','update')->name('update');
-
+Route::get('/admin',function(){
+return view('backend.dashboard');
 });
-
-Route::fallback(function(){
-    
-});
-
